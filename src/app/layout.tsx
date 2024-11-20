ce4b327ec185661139f7ddcdbcb063a4432bb5da
GRACENOBLE <gracenoble72@gmail.com>
2024-11-20 13:50:55 +0300

    feat(font):used variable poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,18 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+const poppins = localFont({
+  src: "./fonts/Poppins.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         {children}
       </body>
